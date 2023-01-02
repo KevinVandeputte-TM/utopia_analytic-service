48b9c5358b9a4d2a08fbd54ec4e86d7f82f32463
VanbroekhovenStef <r0630975@student.thomasmore.be>
2023-01-02 16:50:15 +0100

    first comit

---
+++ src/main/java/fact/it/utopia_visitservice/UtopiaVisitserviceApplication.java
@@ -0,0 +1,13 @@
+package fact.it.utopia_visitservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtopiaVisitserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtopiaVisitserviceApplication.class, args);
+	}
+
+}
