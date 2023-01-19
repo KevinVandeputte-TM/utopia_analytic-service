9924942ec43248c9e8ddc9320bbdbfb408426771
VanbroekhovenStef <r0630975@student.thomasmore.be>
2023-01-19 19:22:03 +0100

    poging 2

--- src/main/java/fact/it/utopia_visitservice/UtopiaVisitserviceApplication.java
+++ src/main/java/fact/it/utopia_visitservice/UtopiaVisitserviceApplication.java
@@ -1,9 +1,12 @@
 package fact.it.utopia_visitservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class UtopiaVisitserviceApplication {
 
 	public static void main(String[] args) {
